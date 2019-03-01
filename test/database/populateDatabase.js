0639488f6ce2a59bf27eefc846e25e942da0c186
narcisneacsu194 <narcisneacsu194@gmail.com>
2019-03-01 10:17:33 +0200

    Cleaned the code of the populateDatabase.js file using eslint.

--- test/database/populateDatabase.js
+++ test/database/populateDatabase.js
@@ -1,17 +1,18 @@
 const { ObjectID } = require('mongodb');
 const { Board } = require('../../models/Board');
 const { Thread } = require('../../models/Thread');
+
 const date = Date.now();
 
 const boards = [
   {
     _id: new ObjectID(),
-    name: 'board1'
+    name: 'board1',
   },
   {
     _id: new ObjectID(),
-    name: 'board2'
-  }
+    name: 'board2',
+  },
 ];
 
 const threads = [
@@ -23,19 +24,19 @@ const threads = [
     delete_password: 'password2',
     boardName: 'board1',
     replies: [
-        {
-            _id: new ObjectID(),
-            text: 'reply1',
-            created_on: date,
-            delete_password: 'password3'
-        },
-        {
-            _id: new ObjectID(),
-            text: 'reply2',
-            created_on: date,
-            delete_password: 'password3'
-        }
-    ]
+      {
+        _id: new ObjectID(),
+        text: 'reply1',
+        created_on: date,
+        delete_password: 'password3',
+      },
+      {
+        _id: new ObjectID(),
+        text: 'reply2',
+        created_on: date,
+        delete_password: 'password3',
+      },
+    ],
   },
   {
     _id: new ObjectID(),
@@ -45,20 +46,20 @@ const threads = [
     delete_password: 'password2',
     boardName: 'board1',
     replies: [
-        {
-            _id: new ObjectID(),
-            text: 'reply3',
-            created_on: date,
-            delete_password: 'password3'
-        },
-        {
-            _id: new ObjectID(),
-            text: 'reply4',
-            created_on: date,
-            delete_password: 'password3'
-        }
-    ]
-  }
+      {
+        _id: new ObjectID(),
+        text: 'reply3',
+        created_on: date,
+        delete_password: 'password3',
+      },
+      {
+        _id: new ObjectID(),
+        text: 'reply4',
+        created_on: date,
+        delete_password: 'password3',
+      },
+    ],
+  },
 ];
 
 const populateBoardCollection = (done) => {
@@ -74,5 +75,5 @@ const populateThreadCollection = (done) => {
 };
 
 module.exports = {
-  populateBoardCollection, populateThreadCollection, boards, threads
+  populateBoardCollection, populateThreadCollection, boards, threads,
 };
